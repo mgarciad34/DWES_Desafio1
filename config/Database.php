adfe76033f50af701d251f4f38aa743836cd9891
Manuel García <145276674+mgarciad34@users.noreply.github.com>
2023-10-09 17:32:15 +0200

    Estructura Inicial

---
+++ config/Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database{
+
+    private $conexion;
+
+    public function __construct() {
+        $this->conexion = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+        if ($this->conexion->connect_error) {
+            die("Error de conexión a la base de datos: " . $this->conexion->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->conexion;
+    }
+   
+}
+?>
---
+++ config/constantes.php
@@ -0,0 +1,8 @@
+<?php
+// constants.php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'buscaminas');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+?>
