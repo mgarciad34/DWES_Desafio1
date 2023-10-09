117d8330f1acd5dd40eb8d27d745600e98de0add
Manuel García <145276674+mgarciad34@users.noreply.github.com>
2023-10-09 21:19:50 +0200

    Nuevas funciones administrador creadas

--- config/Database.php
+++ config/Database.php
@@ -1,12 +1,15 @@
 <?php
+require '../config/constantes.php';
 class Database{
 
     private $conexion;
 
     public function __construct() {
         $this->conexion = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        
 
         if ($this->conexion->connect_error) {
+            
             die("Error de conexión a la base de datos: " . $this->conexion->connect_error);
         }
     }
