16bf49dda73b2a14d785c6970d03bb28bfd0bb04
Manuel García <145276674+mgarciad34@users.noreply.github.com>
2023-10-12 08:56:34 +0200

    Login funcional y rol guardado dentro de la sesion

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start(); // Inicia la sesión
+
 header("Content-Type: application/json");
 
 require 'config/routes.php';
@@ -10,15 +12,18 @@ $requestMethod = $_SERVER["REQUEST_METHOD"];
 $paths = $_SERVER['REQUEST_URI'];
 $argus = explode('/', $paths);
 unset($argus[0]);
-
-//Conexion con nuestra BBDD
 $rol = null;
 $db = new Database();
 
-
 switch ($requestMethod) {
     case 'GET':
-       
+       // En una solicitud GET, puedes acceder al valor de $rol almacenado en la sesión.
+    if (isset($_SESSION['rol'])) {
+      $rol = $_SESSION['rol'];
+      echo json_encode(["rol" => $rol]);
+  } else {
+      echo json_encode(["message" => "No se ha iniciado sesión"]);
+  }
       break;
 
     case 'POST':
@@ -28,12 +33,23 @@ switch ($requestMethod) {
         "password": "admin"
       }
       */
-      if(str_contains(LOGIN, $paths)){
+      if (str_contains(LOGIN, $paths)) {
         $requestBody = file_get_contents("php://input");
         $data = json_decode($requestBody);
-        $rol = databaseController::iniciarSesion($db->getConnection(), $data->email, $data->password);    
-        echo $rol;
-      }
+        
+        //isset -> Comprueba si una variable esta definida y no es nula
+        if ($data !== null && isset($data->email) && isset($data->password)) {
+            $rol = databaseController::iniciarSesion($db->getConnection(), $data->email, $data->password);
+            // Después de asignar $rol
+            echo "Rol después de iniciar sesión: " . $rol;
+
+            // Almacenar en la sesión
+            $_SESSION['rol'] = $rol;
+          } else {
+            // Si los datos JSON no se decodificaron correctamente o faltan campos
+            echo json_encode(["error" => "Datos de inicio de sesión incorrectos"]);
+        }
+    }
       break;
     case 'PUT':
       break;
